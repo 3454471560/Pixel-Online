e02e84865737d9dac31262f299d03c5c65860c2a
3454471560 <3454471560@qq.com>
2026-06-03 19:21:10 +0800

    摄像机组件序列化/反序列化补充

--- Game/Component/Camera.h
+++ Game/Component/Camera.h
@@ -24,7 +24,17 @@ namespace Online::Game
             ctx.Write("renderOffsetY", renderTarget.Offset.y);
             ctx.Write("renderSizeX", renderTarget.Size.x);
             ctx.Write("renderSizeY", renderTarget.Size.y);
-            // cameraState 和 setting 可根据需要继续序列化
+            ctx.Write("cameraZoom", cameraState.Zoom);
+            ctx.Write("cameraRotation", cameraState.Rotation);
+            ctx.Write("postColorR", setting.Color.r);
+            ctx.Write("postColorG", setting.Color.g);
+            ctx.Write("postColorB", setting.Color.b);
+            ctx.Write("postColorA", setting.Color.a);
+            ctx.Write("postAlpha", setting.Alpha);
+            ctx.Write("postBlend", setting.Blend);
+            ctx.Write("postLinear", setting.Linear);
+            ctx.Write("postRotation", setting.Rotation);
+            ctx.Write("postFlip", setting.Flip);
         }
 
         void Deserialize(const Online::Serialize::DeserializeContext& ctx) override
@@ -39,6 +49,41 @@ namespace Online::Game
             ctx.Read("renderOffsetY", renderTarget.Offset.y);
             ctx.Read("renderSizeX", renderTarget.Size.x);
             ctx.Read("renderSizeY", renderTarget.Size.y);
+
+            float zoom = 1.0f;
+            float camRot = 0.0f;
+            ctx.Read("cameraZoom", zoom);
+            ctx.Read("cameraRotation", camRot);
+            cameraState.Zoom = zoom;
+            cameraState.Rotation = camRot;
+
+            Online::Core::Color color = Online::Core::Color::White;
+            uint8_t r = color.r, g = color.g, b = color.b, a = color.a;
+            uint8_t alpha = 255;
+            bool blend = true;
+            bool linear = true;
+            float postRot = 0.0f;
+            bool flip = false;
+
+            ctx.Read("postColorR", r);
+            ctx.Read("postColorG", g);
+            ctx.Read("postColorB", b);
+            ctx.Read("postColorA", a);
+            ctx.Read("postAlpha", alpha);
+            ctx.Read("postBlend", blend);
+            ctx.Read("postLinear", linear);
+            ctx.Read("postRotation", postRot);
+            ctx.Read("postFlip", flip);
+
+            setting.Color.r = r; 
+            setting.Color.g = g; 
+            setting.Color.b = b; 
+            setting.Color.a = a;
+            setting.Alpha = alpha;
+            setting.Blend = blend;
+            setting.Linear = linear;
+            setting.Rotation = postRot;
+            setting.Flip = flip;
         }
 
     public:
